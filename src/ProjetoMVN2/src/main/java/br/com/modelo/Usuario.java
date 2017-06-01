babbff5dee2c0b2be8bae62fba0e3ac4efbf6c18
andressalhenriques <andressalhenriques@gmail.com>
2017-06-01 20:09:45 -0300

    Andressa

--- src/ProjetoMVN2/src/main/java/br/com/modelo/Usuario.java
+++ src/ProjetoMVN2/src/main/java/br/com/modelo/Usuario.java
@@ -1,8 +1,55 @@
 
 package br.com.modelo;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nome;
+    
     private String usuario;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
       
 }
