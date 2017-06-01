83ba0fe17ae487fc5cb1638f763e55dfbf689058
andressalhenriques <andressalhenriques@gmail.com>
2017-06-01 12:42:34 -0300

    CadastroProduto-Andressa

---
+++ src/ProjetoMVN2/src/main/java/br/com/DAO/ProdutosDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.DAO;
+
+import br.com.modelo.Produto;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Andressa
+ */
+public class ProdutosDAO {
+
+    public void persist(Produto produto) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("trabalho");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(produto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+    
+    
+}
